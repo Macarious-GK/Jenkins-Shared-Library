45a3b58a159db559e47280d45f5ea9658bb9b3e9
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-11 05:45:48 +0300

    change snyktest

--- vars/snykTestDockerImage.groovy
+++ vars/snykTestDockerImage.groovy
@@ -2,19 +2,18 @@ def call(Map config = [:]) {
     if (!imageName) {
         error "Image name must be provided for Snyk testing."
     }
-    echo "Running Snyk Container Test for image: ${config.image}"
-    sh '''
-        snyk container test ${config.image} --file=Dockerfile
-                        
+    echo "Running Snyk Container Test for image: ${config.imageName}"
+    sh """
+        snyk container test ${config.imageName} --file=Dockerfile
+        
         # If vulnerabilities are found, print the issues and fail the pipeline
         if [ $? -ne 0 ]; then
             echo "Snyk found vulnerabilities in the Docker image:"
-            snyk container test ${config.image} --file=Dockerfile --json | jq .
+            snyk container test ${config.imageName} --file=Dockerfile --json | jq .
             echo "Failing the pipeline due to Snyk vulnerabilities"
             exit 1
         else
             echo "No vulnerabilities found by Snyk"
         fi
-        '''
-    
+    """
 }
