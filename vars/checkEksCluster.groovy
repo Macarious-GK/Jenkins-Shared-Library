dac6828a2b6da00e8ccb71661d874598fa13c752
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-11 18:19:59 +0300

    edit deploy

--- vars/checkEksCluster.groovy
+++ vars/checkEksCluster.groovy
@@ -4,6 +4,28 @@ def call(String credentialsId, String clusterName, String reginCode) {
         sh """
             aws eks update-kubeconfig --name ${clusterName} --region ${reginCode}
             kubectl get svc
+            echo "Applying namespace.yaml..."
+            kubectl apply -f namespace.yaml
+
+            echo "Applying pv.yaml..."
+            kubectl apply -f pv.yaml
+
+            echo "Applying pvc.yaml..."
+            kubectl apply -f pvc.yaml
+
+            echo "Applying job.yaml..."
+            kubectl apply -f job.yaml
+
+            echo "Applying service.yaml..."
+            kubectl apply -f service.yaml
+
+            echo "Applying ingress.yaml..."
+            kubectl apply -f ingress.yaml
+
+            echo "Applying deployment.yaml..."
+            kubectl apply -f deployment.yaml
+            
+            kubectl get svc -n macarious
             
             """
     }
