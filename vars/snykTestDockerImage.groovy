1815cba10711ef84540a0c8f7db22371e1265e0d
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-11 05:30:16 +0300

    create checkEksCluste

--- vars/snykTestDockerImage.groovy
+++ vars/snykTestDockerImage.groovy
@@ -4,12 +4,12 @@ def call(String imageName) {
     }
     echo "Running Snyk Container Test for image: ${imageName}"
     sh '''
-        snyk container test macarious25siv/books:V4 --file=Dockerfile
+        snyk container test ${imageName} --file=Dockerfile
                         
         # If vulnerabilities are found, print the issues and fail the pipeline
         if [ $? -ne 0 ]; then
             echo "Snyk found vulnerabilities in the Docker image:"
-            snyk container test macarious25siv/books:V4 --file=Dockerfile --json | jq .
+            snyk container test ${imageName} --file=Dockerfile --json | jq .
             echo "Failing the pipeline due to Snyk vulnerabilities"
             exit 1
         else
