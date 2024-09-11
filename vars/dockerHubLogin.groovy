6f4a67551c11a5412a59893b99d8ce3df714548e
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-11 05:10:06 +0300

    update docker login for hiding creds

--- vars/dockerHubLogin.groovy
+++ vars/dockerHubLogin.groovy
@@ -1,8 +1,7 @@
 def call(String credentialsId) {
     withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         echo "Logging in to Docker Hub..."
-        sh '''
-            echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-        '''
+       
+        sh 'echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin'
     }
 }
