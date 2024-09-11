5b7775b8b72aeca897cca8c8aab05aa678175e36
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-11 18:07:26 +0300

    edit deploy

--- vars/checkEksCluster.groovy
+++ vars/checkEksCluster.groovy
@@ -3,6 +3,11 @@ def call(String credentialsId, String clusterName, String reginCode) {
         // Check if the EKS cluster exists
         sh """
             aws eks update-kubeconfig --name ${clusterName} --region ${reginCode}
+            kubectl get svc
+            kubectl apply -f namespace.yaml
+
+            echo "Applying pv.yaml..."
+            kubectl apply -f pv.yaml
             """
     }
 }
