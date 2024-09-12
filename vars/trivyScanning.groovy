d19cb0ab0cf8b00eae662cbf8ff4f34431d0a5a8
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-09-12 17:27:47 +0300

    test Trivy

--- vars/trivyScanning.groovy
+++ vars/trivyScanning.groovy
@@ -18,25 +18,45 @@
 //     """
 // }
 
+// def call(Map config = [:]) {
+//     if (!config.imageName) {
+//         error "Image name must be provided for Trivy testing."
+//     }
+    
+//     echo "Running Trivy Container Test for image: ${config.imageName}"
+    
+//     // Run Trivy scan and capture the output
+//     def scanOutput = sh(script: "trivy image --severity HIGH,CRITICAL ${config.imageName} --format json", returnStdout: true).trim()
+
+//     // Parse the JSON output to check for HIGH severity vulnerabilities
+//     def vulnerabilities = sh(script: "echo '${scanOutput}' | jq '.[] | select(.Severity == \"HIGH\")'", returnStdout: true).trim()
+
+//     // Check if vulnerabilities were found
+//     if (vulnerabilities) {
+//         echo "Trivy found HIGH severity vulnerabilities in the Docker image:"
+//         echo "${scanOutput}"
+//         error "Failing the pipeline due to Trivy vulnerabilities"
+//     } else {
+//         echo "No HIGH severity vulnerabilities found by Trivy"
+//     }
+// }
+
+
 def call(Map config = [:]) {
     if (!config.imageName) {
         error "Image name must be provided for Trivy testing."
     }
-    
-    echo "Running Trivy Container Test for image: ${config.imageName}"
-    
-    // Run Trivy scan and capture the output
-    def scanOutput = sh(script: "trivy image --severity HIGH,CRITICAL ${config.imageName} --format json", returnStdout: true).trim()
 
-    // Parse the JSON output to check for HIGH severity vulnerabilities
-    def vulnerabilities = sh(script: "echo '${scanOutput}' | jq '.[] | select(.Severity == \"HIGH\")'", returnStdout: true).trim()
+    echo "Running Trivy scan for image: ${config.imageName}"
 
-    // Check if vulnerabilities were found
-    if (vulnerabilities) {
-        echo "Trivy found HIGH severity vulnerabilities in the Docker image:"
-        echo "${scanOutput}"
-        error "Failing the pipeline due to Trivy vulnerabilities"
+    // Run Trivy scan and check for HIGH severity vulnerabilities
+    def scanResult = sh(script: "trivy image --severity HIGH,CRITICAL ${config.imageName} | tee", returnStatus: true)
+
+    // If any HIGH severity vulnerabilities are found, fail the pipeline
+    if (scanResult.contains("HIGH")) {
+        echo "Trivy found HIGH severity vulnerabilities in the Docker image."
+        error "Failing the pipeline due to HIGH severity vulnerabilities."
     } else {
-        echo "No HIGH severity vulnerabilities found by Trivy"
+        echo "No HIGH severity vulnerabilities found by Trivy."
     }
 }
