0fddd09985b933b668557a6d74937dc533e55a78
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-12-16 00:51:59 +0200

    add installdjangodependency and unittestV2

--- vars/installDjangoDependencies.groovy
+++ vars/installDjangoDependencies.groovy
@@ -2,8 +2,17 @@ def call(){
     sh '''
         echo "Setting up virtual environment and installing dependencies..."
         python3 -m venv venv
-        source venv/bin/activate
-        pip install -r requirements.txt
-        deactivate
+
+        # Activate virtual environment using bash
+        bash -c "source venv/bin/activate && pip install -r requirements.txt && deactivate"
+        result=$?
+
+        if [ $result -eq 0 ]; then
+            echo "installing dependencies successfully."
+        else
+            echo "installing dependencies failed."
+            exit 1
+        fi
+        '''
         '''
 }
