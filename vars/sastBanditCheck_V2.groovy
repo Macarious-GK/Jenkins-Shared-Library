d5b90889b3825c45df58a0dab883d457212900bc
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-12-20 03:19:04 +0200

    add sastBanditCheck_V2

---
+++ vars/sastBanditCheck_V2.groovy
@@ -0,0 +1,18 @@
+def call() {
+    sh '''
+        echo "Running Bandit for security checks..."
+        bandit -r . -f json -o bandit_Report.json
+
+        # Echo the Bandit report for visibility in Jenkins log
+        echo "Bandit security report:"
+        cat bandit_Report.json
+
+        # Check if Bandit found any issues
+        if grep '"issue_severity":' bandit_Report.json > /dev/null; then
+            echo "Failed from SAST stage: Security issues detected by Bandit."
+
+        else
+            echo "SAST stage passed successfully: No issues found by Bandit."
+        fi
+    '''
+}
