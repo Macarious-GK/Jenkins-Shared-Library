836e3e72fae3d7dae3d4a02b0f2ab26897c7367c
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-12-20 04:05:57 +0200

    add argoCD_Sync

--- vars/argoCDSync.groovy
+++ vars/argoCDSync.groovy
@@ -1,16 +1,5 @@
 def call(){
-    sh """
-                        argocd login $ARGOCD_SERVER --token $ARGOCD_AUTH_TOKEN --insecure
-                    """
-                    
-                    // Manually sync the ArgoCD application
-                    // Replace 'your-app-name' with the name of your ArgoCD application
-                    sh """
-                        argocd app sync your-app-name
-                    """
-                    
-                    // Optional: Check the sync status
-                    sh """
-                        argocd app wait your-app-name --sync
-                    """
+    sh '''
+        sleep 20
+        '''
 }
