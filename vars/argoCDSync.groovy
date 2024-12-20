000a6f4f53bef12ccf44e00985c80b6b3b507fc9
Macarious-GK <125499309+Macarious-GK@users.noreply.github.com>
2024-12-20 05:59:19 +0200

    update nargocd

--- vars/argoCDSync.groovy
+++ vars/argoCDSync.groovy
@@ -1,10 +1,10 @@
-def call(String server, String usernameCredId, String passwordCredId, String appName) {
+def call(Map config = [:]) {
     withCredentials([
-        usernamePassword(credentialsId: credsId, usernameVariable: 'ARGOCD_USERNAME', passwordVariable: 'ARGOCD_PASSWORD')
+        usernamePassword(credentialsId: config.argocd_creds, usernameVariable: 'ARGOCD_USERNAME', passwordVariable: 'ARGOCD_PASSWORD')
     ]) {
         sh """
-            argocd login ${server} --username ${ARGOCD_USERNAME} --password ${ARGOCD_PASSWORD} --insecure
-            argocd app sync ${appName} --wait
+            argocd login ${config.server} --username ${ARGOCD_USERNAME} --password ${ARGOCD_PASSWORD} --insecure
+            argocd app sync ${config.appName} --wait
         """
     }
 }
